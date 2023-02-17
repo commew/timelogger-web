e388f7196f3da163d88b46d253f97cac29e5a21f
keitakn <keita.koga.work@gmail.com>
2023-02-17 22:28:40 +0900

    :art: #5 Mantineを利用する為に必要な設定を追加
    https://mantine.dev/guides/next/

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,30 @@
-import '@/styles/globals.css';
+import { MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 };
 
 export default App;
