09cfc5fe9bf9dbb5e977daeb963c1473bbe36ff8
keitakn <keita.koga.work@gmail.com>
2023-03-18 00:50:51 +0900

    :art: #13 GitHubのユーザー検索ページはログイン状態でないと閲覧出来ないように変更

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,8 +1,27 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/react';
+import { appUrls } from '@/features';
 import { GitHubAccountSearchTemplate } from '@/templates';
 
 const SearchPage: NextPage = () => {
   return <GitHubAccountSearchTemplate />;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: appUrls.login.path,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default SearchPage;
