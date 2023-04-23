87dd86a34afc50192d950ae15f0d7ed9848ae73b
c501306014 <c501306014@gmail.com>
2023-04-23 19:21:34 +0900

    #47 Linterを適用した

--- src/components/TaskMeasurementCategoryButton/TaskMeasurementCategoryButton.tsx
+++ src/components/TaskMeasurementCategoryButton/TaskMeasurementCategoryButton.tsx
@@ -48,7 +48,7 @@ export const TaskMeasurementCategoryButton: FC<Props> = ({ name }) => {
       </Box>
       {hovered ? (
         <Image
-          role='button'
+          role="button"
           src={hoveredImageSrc}
           alt={'タスクの計測を開始'}
           width={32}
