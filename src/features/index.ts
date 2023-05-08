32501b1d803f5f92b6bceb913b27d12917382197
keitakn <keita.koga.work@gmail.com>
2023-05-08 16:30:35 +0900

    :art: #73 アカウントの作成・取得用の関数IF、APIのURL取得関数をpackage外にexport

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,4 +1,4 @@
-export { appUrls, appUrl } from './url';
+export { appUrls, appUrl, getBackendApiUrl } from './url';
 export { GitHubAccountNotFoundError } from './gitHub';
 export type { GitHubAccount, FetchGitHubAccount } from './gitHub';
 export { httpStatusCode } from './httpStatusCode';
@@ -7,3 +7,4 @@ export type { GoogleTagManagerId } from './googleTagManagerId';
 export { getNullableGoogleTagManagerId } from './googleTagManagerId';
 export { isOidcProvider } from './auth';
 export type { OidcProvider } from './auth';
+export type { CreateAccount, FindAccount } from './account';
