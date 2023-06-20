c5f4c1bab0b0b3b632ac36877b3902a9a5cbe034
c501306014 <c501306014@gmail.com>
2023-06-20 12:21:19 +0900

    #81 停止中のタスクアイテムコンポーネントを実装

---
+++ src/components/StoppedTaskItem/StoppedTaskItem.stories.tsx
@@ -0,0 +1,18 @@
+import type { ComponentStoryObj } from '@storybook/react';
+import { StoppedTaskItem } from './StoppedTaskItem';
+
+const story = {
+  component: StoppedTaskItem,
+};
+
+export default story;
+
+type Story = ComponentStoryObj<typeof StoppedTaskItem>;
+
+export const Default: Story = {
+  args: {
+    categoryName: 'カテゴリ名',
+    categoryGroupName: 'グループ名',
+    isMeasuring: true,
+  },
+};
