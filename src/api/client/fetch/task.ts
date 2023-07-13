0c5eec9fa1dd595971064b35ecc8356bd432a140
c501306014 <c501306014@gmail.com>
2023-07-13 22:43:32 +0900

    #53 タスク計測を停止する API リクエストを実装した

--- src/api/client/fetch/task.ts
+++ src/api/client/fetch/task.ts
@@ -1,4 +1,4 @@
-import type { Task, CreateTask } from '@/features';
+import type { Task, CreateTask, StopTask } from '@/features';
 import {
   getBackendApiUrl,
   httpStatusCode,
@@ -26,6 +26,40 @@ export const createTask: CreateTask = async (dto) => {
   });
 
   if (response.status !== httpStatusCode.created) {
+    throw new UnexpectedFeatureError(
+      `failed to createTask. status: ${
+        response.status
+      }, body: ${await response.text()}`
+    );
+  }
+
+  const task = (await response.json()) as Task;
+  if (!isTask(task)) {
+    throw new InvalidResponseBodyError(
+      `responseBody is not in the expected format. body: ${JSON.stringify(
+        task
+      )}`
+    );
+  }
+
+  return task;
+};
+
+export const stopTask: StopTask = async (dto) => {
+  const { taskId, appToken } = dto;
+
+  const response = await fetch(
+    getBackendApiUrl('tasks/{taskId}/stop').replace('{taskId}', `${taskId}`),
+    {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Bearer ${appToken}`,
+        Prefer: 'code=200, example=ExampleSuccess',
+      },
+    }
+  );
+
+  if (response.status !== httpStatusCode.ok) {
     throw new UnexpectedFeatureError(
       `failed to createTask. status: ${
         response.status
