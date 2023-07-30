2395a11dbe2c61795964cae2e8940427915d28fd
c501306014 <c501306014@gmail.com>
2023-07-30 16:06:17 +0900

    #60 featuresでのexport漏れを修正した

--- src/features/index.ts
+++ src/features/index.ts
@@ -14,15 +14,17 @@ export { isOidcProvider, createBackendApiBasicAuthCredential } from './auth';
 export type { OidcProvider } from './auth';
 export { isAccount } from './account';
 export type { Account, CreateAccount, FindAccount } from './account';
-export { isTask, isRecordingTasks } from './task';
+export { isTask, isRecordingTasks, isPendingTasks } from './task';
 export type {
   Task,
   TaskRecording,
+  TaskPending,
   Tasks,
   CreateTask,
   StopTask,
   CompleteTask,
   FetchTasksRecording,
+  FetchTasksPending,
 } from './task';
 export {
   InvalidResponseBodyError,
