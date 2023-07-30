ab4a2475a23b4d96835ddb0338210e122355be9b
c501306014 <c501306014@gmail.com>
2023-07-30 16:03:07 +0900

    #60 停止中のタスクに関する型定義・関数インターフェースを実装した

--- src/features/task/index.ts
+++ src/features/task/index.ts
@@ -1,10 +1,12 @@
-export { isTask, isRecordingTasks } from './task';
+export { isTask, isRecordingTasks, isPendingTasks } from './task';
 export type {
   Task,
   TaskRecording,
+  TaskPending,
   Tasks,
   CreateTask,
   StopTask,
   CompleteTask,
   FetchTasksRecording,
+  FetchTasksPending,
 } from './task';
--- src/features/task/task.ts
+++ src/features/task/task.ts
@@ -22,12 +22,19 @@ type FetchTasksRecordingDto = {
   appToken: string;
 };
 
+type FetchTasksPendingDto = {
+  appToken: string;
+};
+
 export type Task = components['schemas']['Task'];
 export type Tasks = {
   tasks?: Task[];
 };
 export type TaskRecording = Omit<Task, 'status'> & {
   status: 'recording';
+};
+export type TaskPending = Omit<Task, 'status'> & {
+  status: 'pending';
 };
 
 const taskSchema = z.object({
@@ -49,6 +56,12 @@ const taskRecordingSchema = taskSchema.extend({
 
 const tasksRecordingSchema = z.array(taskRecordingSchema);
 
+const taskPendingSchema = taskSchema.extend({
+  status: z.literal('pending'),
+});
+
+const tasksPendingSchema = z.array(taskPendingSchema);
+
 export const isTask = (value: unknown): value is Task => {
   const result = taskSchema.safeParse(value);
 
@@ -58,9 +71,15 @@ export const isTask = (value: unknown): value is Task => {
 export const isRecordingTasks = (value: unknown): value is TaskRecording[] => {
   return tasksRecordingSchema.safeParse(value).success;
 };
+export const isPendingTasks = (value: unknown): value is TaskPending[] => {
+  return tasksPendingSchema.safeParse(value).success;
+};
 export type CreateTask = (dto: CreateTaskDto) => Promise<Task>;
 export type StopTask = (dto: StopTaskDto) => Promise<Task>;
 export type CompleteTask = (dto: CompleteTaskDto) => Promise<Task>;
 export type FetchTasksRecording = (
   dto: FetchTasksRecordingDto
 ) => Promise<TaskRecording[]>;
+export type FetchTasksPending = (
+  dto: FetchTasksPendingDto
+) => Promise<TaskPending[]>;
