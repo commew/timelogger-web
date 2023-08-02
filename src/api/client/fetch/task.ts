fcf23e555a69ffd5f6801914f8b357c4aee757b7
c501306014 <c501306014@gmail.com>
2023-08-02 11:26:52 +0900

    #101 クライアントコンポーネントで使用するタスク作成用のリクエスト処理を実装した

---
+++ src/api/client/fetch/task.ts
@@ -0,0 +1,50 @@
+import {
+  getAppApiUrl,
+  httpStatusCode,
+  InvalidResponseBodyError,
+  isTask,
+  UnexpectedFeatureError,
+} from '@/features';
+import type { CreateTask, Task } from '@/features';
+import { type operations } from '@/openapi/schema';
+
+export const createTask: CreateTask = async (dto) => {
+  const { taskCategoryId, status, startAt } = dto;
+
+  const requestBody: operations['postTasks']['requestBody'] = {
+    content: {
+      'application/json': {
+        taskCategoryId,
+        status,
+        startAt,
+      },
+    },
+  };
+
+  const response = await fetch(getAppApiUrl('tasks'), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestBody.content['application/json']),
+  });
+
+  if (response.status !== httpStatusCode.created) {
+    throw new UnexpectedFeatureError(
+      `failed to createTask. status: ${
+        response.status
+      }, body: ${await response.text()}`
+    );
+  }
+
+  const task = (await response.json()) as Task;
+  if (!isTask(task)) {
+    throw new InvalidResponseBodyError(
+      `responseBody is not in the expected format. body: ${JSON.stringify(
+        task
+      )}`
+    );
+  }
+
+  return task;
+};
