af1cf94662d9e1b73192e2713c5732c132f6b0c1
c501306014 <c501306014@gmail.com>
2023-08-14 16:42:14 +0900

    Revert "#101 Edge API Routes を導入してみた"
    ビルドが失敗したのでリバートします。This reverts commit df3e5b83b3bf242fee7f54227cde2af16f14233c.

--- src/pages/api/tasks/create.ts
+++ src/pages/api/tasks/create.ts
@@ -6,10 +6,6 @@ import type { Task } from '@/features';
 import { isNextApiRequestBodyOfCreateTaskDto } from '@/features/task/task';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
-export const config = {
-  runtime: 'edge',
-}
-
 type ErrorData = {
   type: string;
   title: string;
