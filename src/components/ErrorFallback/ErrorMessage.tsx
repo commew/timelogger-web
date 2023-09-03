79e4091bccb9e68017e447833f1450dc55f1ec2c
c501306014 <c501306014@gmail.com>
2023-09-03 12:16:08 +0900

    #112 エラーメッセージにタスクグループ・カテゴリが見つからなかった場合のテキストを追加した

--- src/components/ErrorFallback/ErrorMessage.tsx
+++ src/components/ErrorFallback/ErrorMessage.tsx
@@ -1,10 +1,22 @@
 import type { FC } from 'react';
 import { Alert } from '@mantine/core';
-import { GitHubAccountNotFoundError } from '@/features';
+import {
+  GitHubAccountNotFoundError,
+  TaskCategoryNotFoundError,
+  TaskGroupNotFoundError,
+} from '@/features';
 
 const createDisplayErrorMessage = (error: Error) => {
   if (error instanceof GitHubAccountNotFoundError) {
     return 'GitHubアカウントは見つかりませんでした。';
+  }
+
+  if (error instanceof TaskGroupNotFoundError) {
+    return 'タスクグループが見つかりませんでした。';
+  }
+
+  if (error instanceof TaskCategoryNotFoundError) {
+    return 'タスクカテゴリが見つかりませんでした。';
   }
 
   return '予期せぬエラーが発生しました。申し訳ありませんが、しばらく時間が経ってからお試し下さい。';
