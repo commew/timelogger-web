a103f9cf939cb944e5815d3ea575c7ba2d6af2dd
keitakn <keita.koga.work@gmail.com>
2023-09-12 22:23:49 +0900

    :fire: #120 不要なサンプル用のComponentを削除

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { TitleText } from './TitleText';
 export { GitHubAccountSearch } from './GitHubAccountSearch';
 export { ErrorFallback } from './ErrorFallback';
-export { HeaderMenu } from './HeaderMenu';
 export { HeaderNavigation } from './HeaderNavigation';
 export { SideBar } from './SideBar';
 export { TaskMeasurementCategoryButton } from './TaskMeasurementCategoryButton';
